7ca9b52b820bf021cadeaf543da7fb4a2eae3e55
tylermckenzie <tylrmckenz@gmail.com>
2017-02-27 13:26:42 -0600

    big models push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  has_attached_file :profile_image, styles: { large: "500X500>", medium: "300X300>", thumb: "100X100>" }, default_url: "/images/missing.png"
+  validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\z/
   has_many :projects
+  has_many :skills
+  has_many :contact_types
 end
