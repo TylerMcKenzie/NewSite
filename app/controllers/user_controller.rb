b7cc8b431542af93d424740d0ab625887b344683
tylermckenzie <tylrmckenz@gmail.com>
2017-02-28 12:13:26 -0600

    begin user construction

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,12 +15,19 @@ class UserController < ApplicationController
   end
 
   def create
-    p params
+    @user = User.new(user_params)
+    p "=============PARAMS====================="
+    p user_params
+    p "=============USER======================="
+    p @user
+    # p "=============PROJECT_PARAMS============="
+    # p @user.projects << new(project_params)
+    p "======================================"
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :about)
+    params.require(:user).permit(:name, :email, :about, :profile_image, { skills_attributes: [ :name, :proficiency ] }, { projects_attributes: [ :name, :url, :completed, { screenshots: :image } ] }, { contact_types_attributes: [:name, :url] })
   end
 end
