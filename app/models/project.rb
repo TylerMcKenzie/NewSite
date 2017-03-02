89cc1d78ab4eb773d2b8b4f846b63d569bc47b43
tylermckenzie <tylrmckenz@gmail.com>
2017-03-02 15:34:31 -0600

    add paperclip touches, onto admin stuff

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :screenshots
+  has_many :screenshots, :dependent => :delete_all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   has_attached_file :profile_image, styles: { large: "500X500>", medium: "300X300>", thumb: "100X100>" }, default_url: "/images/missing.png"
-  validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\z/
-  has_many :projects
-  has_many :skills
-  has_many :contact_types
-  accepts_nested_attributes_for :projects, :skills, :contact_types, allow_destroy: true, reject_if: :all_blank 
+
+  validates_attachment_content_type :profile_image, { content_type: ["image/jpeg", "image/png", "image/svg+xml"] }
+
+  has_many :projects, :dependent => :delete_all
+  has_many :skills, :dependent => :delete_all
+  has_many :contact_types, :dependent => :delete_all
+  accepts_nested_attributes_for :projects, :skills, :contact_types, allow_destroy: true, reject_if: :all_blank
+
+  def completed_projects
+    []
+  end
+
+  def incomplete_projects
+    []
+  end
 end
