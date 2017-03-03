409320b5290d9632225dd05d4b3f38ac49205519
tylermckenzie <tylrmckenz@gmail.com>
2017-03-03 00:06:08 -0600

    update branch

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,17 +16,12 @@ class UserController < ApplicationController
 
   def create
     @user = UserCreationService.new(user_params)
+    
     if @user.save
       redirect_to '/'
     else
       @user.errors.full_messages
     end
-    # p "=============PARAMS====================="
-    # p user_params
-    # p @user
-    # # p "=============PROJECT_PARAMS============="
-    # # p @user.projects << new(project_params)
-    # p "======================================"
   end
 
   private
